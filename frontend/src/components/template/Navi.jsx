d1bf12cb9bbc184451e3cd4a9bdf99d29f364c82
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-26 11:28:26 -0300

    add structure and styles in Navi

--- frontend/src/components/template/Navi.jsx
+++ frontend/src/components/template/Navi.jsx
@@ -5,8 +5,15 @@ import './Navi.css';
 function Navi(props){
 
     return(
-        <aside className="menu=area">
-            Navegação
+        <aside className="menu-area">
+            <nav className="menu">
+                <a href="#/">
+                    <i className="fa fa-home"></i> Inicio
+                </a>
+                <a href="#/users">
+                    <i className="fa fa-users"></i> Usuários
+                </a>
+            </nav>
         </aside>
     );
 }
