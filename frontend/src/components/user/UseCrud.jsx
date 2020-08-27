a6f0b58a1b461c9a07d229d449825069f59da0f5
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-27 16:19:45 -0300

    add adjust in user part 1

--- frontend/src/components/user/UseCrud.jsx
+++ frontend/src/components/user/UseCrud.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Main from '../template/Main';
 import { Component } from 'react';
+import axios from 'axios';
+
+import Main from '../template/Main';
 
 const headerProps = {
     icon: 'users',
@@ -8,7 +10,37 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Remover'
 }
 
+const baseUrl = 'http://localhost:3333/users';
+
+const initialState = {
+    user: {name:'', email:''},
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = {...initialState}
+
+    clear() {
+        this.setState({user: initialState.user});
+    }
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data);
+                this.setState({user: initialState.user, list})
+            })
+    }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter(user => user.id !== user.id)
+        list.unshift(user);
+        return list
+    }
 
     render() {
         return(
